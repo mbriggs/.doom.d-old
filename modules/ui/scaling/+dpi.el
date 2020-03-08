b58edc2c05816a60cd6f03f1c4c6c6252f7dfa66
Matt Briggs <matt@mattbriggs.net>
2020-03-08 14:03:47 -0400

    font scaling is unnecessary when setting floats

--- modules/ui/scaling/+dpi.el
+++ modules/ui/scaling/+dpi.el
@@ -1,40 +1,38 @@
 ;;; ui/scaling/+dpi.el -*- lexical-binding: t; -*-
 
-(defun mb--dpi ()
-  (let* ((attrs (car (display-monitor-attributes-list)))
-          (size (assoc 'mm-size attrs))
-          (sizex (cadr size))
-          (res (cdr (assoc 'geometry attrs)))
-          (resx (- (caddr res) (car res))))
-    (catch 'exit
-                                        ; term
-      (unless sizex
-        (throw 'exit 10))
-      (* (/ (float resx) sizex) 25.4))))
+;; (defun mb--dpi ()
+;;   (let* ((attrs (car (display-monitor-attributes-list)))
+;;           (size (assoc 'mm-size attrs))
+;;           (sizex (cadr size))
+;;           (res (cdr (assoc 'geometry attrs)))
+;;           (resx (- (caddr res) (car res))))
+;;     (catch 'exit
+;;                                         ; term
+;;       (unless sizex
+;;         (throw 'exit 10))
+;;       (* (/ (float resx) sizex) 25.4))))
 
-(defun mb--preferred-font-size ()
-  (let ((dpi (mb--dpi)))
-    (cond
-      ((< dpi 110) 18)
-      ((< dpi 160) 26)
-      ((< dpi 330) 54)
-      (t 54))))
+;; (defun mb--preferred-font-size ()
+;;   (let ((dpi (mb--dpi)))
+;;     (cond
+;;       ((< dpi 110) 18.0)
+;;       ((< dpi 160) 26.0)
+;;       ((< dpi 330) 15.0)
+;;       (t 15.0))))
 
-(defun mb--preferred-big-font-scale ()
-  (let*
-    ((dpi (mb--dpi))
-      (scale (cond
-               ((< dpi 160) 0.8)
-               ((< dpi 330) 0.4)
-               (t 0.4))))
-    (if IS-MAC (+ scale 1)
-      scale)))
+;; (defun mb--preferred-big-font-scale ()
+;;   (let* ((dpi (mb--dpi)))
+;;   (cond
+;;    ((< dpi 160) 1.8)
+;;    ((< dpi 330) 1.4)
+;;    (t 1.4))))
 
 (defun mb-apply-doom-fonts ()
-  (let ((size (mb--preferred-font-size))
-         (big-scale (mb--preferred-big-font-scale)))
+  (let* ((size 15.0) ;(mb--preferred-font-size))
+         (big-scale 1.4) ; (mb--preferred-big-font-scale))
+         (big-size (fround (* big-scale size))))
     (setq doom-font (font-spec :family "JetBrains Mono" :size size))
-    (setq doom-big-font (font-spec :family "JetBrains Mono" :size (* big-scale size)))))
+    (setq doom-big-font (font-spec :family "JetBrains Mono" :size big-size))))
 
 (defun mb/refresh-fonts ()
   (interactive)
